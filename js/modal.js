a5e1f2ab57cd0714028ebac52eee9b7d9c3ea2bf
Soliquidus <tibo.pfeifer@gmail.com>
2021-09-30 20:04:26 +0200

    TP 06 - Fini, JS du TP05 fini

--- js/modal.js
+++ js/modal.js
@@ -7,7 +7,7 @@ const responseFirstName = document.getElementById('resFirstName');
 const birthDate = document.querySelector('#birthDate');
 const responseBirthDate = document.getElementById('resBirthDate');
 
-const socialSecurityNumber = document.querySelector('#socialSecurity');
+const socialSecurity = document.querySelector('#socialSecurity');
 const responseSocialSecurity = document.getElementById('resSocialSecurity');
 
 const address = document.querySelector('#address');
@@ -16,18 +16,26 @@ const responseAddress = document.getElementById('resAddress');
 name.addEventListener('input', updateValue);
 firstName.addEventListener('input', updateValue);
 birthDate.addEventListener('input', updateValue);
-socialSecurityNumber.addEventListener('input', updateValue);
+socialSecurity.addEventListener('input', updateValue);
 address.addEventListener('input', updateValue);
 
 function updateValue(event){
-    responseName.textContent = event.target.value;
-    event.stopPropagation();
-    responseFirstName.textContent = event.target.value;
-    event.stopPropagation();
-    responseBirthDate.textContent = event.target.value;
-    event.stopPropagation();
-    responseSocialSecurity.textContent = event.target.value;
-    event.stopPropagation();
-    responseAddress.textContent = event.target.value;
+    switch (event.srcElement.id) {
+        case 'name':
+            responseName.textContent = event.target.value;
+            break;
+        case 'firstName':
+            responseFirstName.textContent = event.target.value;
+            break;
+        case 'birthDate':
+            responseBirthDate.textContent = event.target.value;
+            break;
+        case 'socialSecurity':
+            responseSocialSecurity.textContent = event.target.value;
+            break;
+        case 'address':
+            responseAddress.textContent = event.target.value;
+            break;
+    }
     event.stopPropagation();
 }
